8a92f6cb772dddee59448aadf665be152013233d
Chhayuth <chhayuth0@gmail.com>
2024-01-31 15:44:11 +0700

    not yet

--- app/(dashboard)/users/data-table.jsx
+++ app/(dashboard)/users/data-table.jsx
@@ -1,17 +1,17 @@
-import { userList } from '@/app/api/users'
-import { DatePicker } from '@/components/date-picker'
 import { RowAction } from '@/components/row-action'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
-import {DropdownMenu, DropdownMenuItem, DropdownMenuTrigger,DropdownMenuContent} from '@/components/ui/dropdown-menu' 
+import {
+  DropdownMenu,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+  DropdownMenuContent
+} from '@/components/ui/dropdown-menu'
 import {
   Pagination,
   PaginationContent,
-  PaginationEllipsis,
   PaginationItem,
-  PaginationLink,
-  PaginationNext,
-  PaginationPrevious
+  PaginationLink
 } from '@/components/ui/pagination'
 import {
   Table,
@@ -22,54 +22,46 @@ import {
   TableRow
 } from '@/components/ui/table'
 import { useEffect, useState } from 'react'
-import { useSelector } from 'react-redux'
 import { SlidersHorizontal } from 'lucide-react'
 
 export const DataTable = ({ data, meta, pagination }) => {
-  const [filter,setFilter] = useState('')
-  // const handleSelect = e => {
-  //   pagination(meta.currentPage, e.target.value)
-  // }
+  const [filter, setFilter] = useState('')
 
   useEffect(() => {
     pagination(meta?.currentPage, filter)
-  },[filter])
+  }, [filter])
 
-  const handleMenuSelect = (value) => {
-    
-  }
+  const handleMenuSelect = value => {}
 
   return (
     <div className='w-full'>
       <div className='flex justify-between items-center py-4 bg-white p-2 my-2 rounded-md border '>
         <Input
-          name="name"
+          name='name'
           placeholder='Filter name or email...'
           className='max-w-sm'
           onChange={e => setFilter(e.target.value)}
         />
 
         <DropdownMenu>
-          <DropdownMenuTrigger>
-            <Button><SlidersHorizontal className='w-4 h-4 mr-2'></SlidersHorizontal> Filter</Button>
+          <DropdownMenuTrigger asChild>
+            <Button variant='secondary'>
+              <SlidersHorizontal className='w-4 h-4 mr-2' />
+              Filter
+            </Button>
           </DropdownMenuTrigger>
 
           <DropdownMenuContent align='end'>
-
             <DropdownMenuItem onClick={() => handleMenuSelect('show')}>
-             Show All User
+              Show All User
             </DropdownMenuItem>
-
             <DropdownMenuItem onClick={() => handleMenuSelect('deleted')}>
-             Show Only Deleted
+              Show Only Deleted
             </DropdownMenuItem>
-
             <DropdownMenuItem onClick={() => handleMenuSelect('available')}>
               Show Available Uer
             </DropdownMenuItem>
-
           </DropdownMenuContent>
-
         </DropdownMenu>
       </div>
       <div className='rounded-md border bg-white'>
@@ -78,12 +70,33 @@ export const DataTable = ({ data, meta, pagination }) => {
             <TableRow>
               <TableHead className='w-[100px]'>No.</TableHead>
               <TableHead>Name</TableHead>
+              <TableHead>Username</TableHead>
               <TableHead>Email</TableHead>
-              <TableHead>Created_at</TableHead>
               <TableHead>Action</TableHead>
             </TableRow>
           </TableHeader>
           <TableBody>
+            {meta?.total > 0 ? (
+              <>
+                {data?.map(item => (
+                  <TableRow key={item.id}>
+                    <TableCell className='font-medium'>{item.id}</TableCell>
+                    <TableCell>{item.name}</TableCell>
+                    <TableCell>{item.username}</TableCell>
+                    <TableCell>{item.email}</TableCell>
+                    <TableCell>
+                      <RowAction />
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </>
+            ) : (
+              <TableRow>
+                <TableCell colSpan={4} className='h-24 text-center'>
+                  No results.
+                </TableCell>
+              </TableRow>
+            )}
           </TableBody>
         </Table>
       </div>
@@ -94,7 +107,6 @@ export const DataTable = ({ data, meta, pagination }) => {
         <div className='space-x-2'>
           <Pagination>
             <PaginationContent>
-
               {meta?.pages?.map(page => (
                 <PaginationItem key={page}>
                   <PaginationLink href='#'>
@@ -107,7 +119,6 @@ export const DataTable = ({ data, meta, pagination }) => {
                   </PaginationLink>
                 </PaginationItem>
               ))}
-
             </PaginationContent>
           </Pagination>
         </div>
